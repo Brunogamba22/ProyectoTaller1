1c38f8e974623cd3fcc6dfdf7d340b4493bfe429
Brunogamba22 <brunogamba439@gmail.com>
2025-04-18 17:23:28 -0300

    agregando imagenes

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -10,6 +10,7 @@
     </footer>
     
     <!-- Scripts JavaScript -->
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> <!-- Bootstrap JS -->
   </body>
 </html>
