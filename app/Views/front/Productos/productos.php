c92dbc7105984f4f78fa0d26823875d3130b5075
Brunogamba22 <brunogamba439@gmail.com>
2025-04-24 18:26:53 -0300

    incorporacion de imagenes

--- app/Views/front/Productos/productos.php
+++ app/Views/front/Productos/productos.php
@@ -15,12 +15,12 @@
         <!-- Grid de Bootstrap: 1 columna en pantallas chicas, 3 en pantallas medianas en adelante -->
         <div class="row row-cols-1 row-cols-md-3 g-4">
             
-            <!-- === Producto 1 === -->
+            <!-- === Producto 1 Remera1=== -->
             <div class="col"> <!-- Una columna del grid -->
             <div class="card h-100"> <!-- Tarjeta que ocupa toda la altura disponible -->
                 
                 <!-- Imagen del producto -->
-                <img src="assets/img/Remera1.webp"alt="Camisa 1">
+                <img src="assets/img/Remera1.webp" class="card-img-top img-producto" alt="remera 1">
                 
                 <!-- Cuerpo de la tarjeta con la información del producto -->
                 <div class="card-body">
@@ -38,10 +38,10 @@
             </div>
             </div>
 
-            <!-- === Producto 2 === -->
+            <!-- === Producto 2 Remera2=== -->
             <div class="col">
             <div class="card h-100">
-                <img src="assets/img/Remera2.webp" class="card-img-top" alt="Remera 2">
+                <img src="assets/img/Remera2.webp" class="card-img-top img-producto" alt="Remera 2">
                 <div class="card-body">
                 <h5 class="card-title">Remera Azul Oversize</h5>
                 <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
@@ -54,10 +54,10 @@
             </div>
             </div>
 
-            <!-- === Producto 3 === -->
+            <!-- === Producto 3 Buzo1 === -->
             <div class="col">
             <div class="card h-100">
-                <img src="assets/img/Buzo1.webp" class="card-img-top" alt="Buzo 1">
+                <img src="assets/img/Buzo1.webp" class="card-img-top img-producto" alt="Buzo 1">
                 <div class="card-body">
                 <h5 class="card-title">Buzo Canguro Gris</h5>
                 <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
@@ -71,10 +71,10 @@
             </div>
 
 
-            <!-- === Producto 4 === -->
+            <!-- === Producto 4 Buzo2=== -->
             <div class="col">
             <div class="card h-100">
-                <img src="assets/img/Buzo2.webp" class="card-img-top" alt="Remera 2">
+                <img src="assets/img/Buzo2.webp" class="card-img-top img-producto" alt="buzo 2">
                 <div class="card-body">
                 <h5 class="card-title">Buzo Canguro Negro</h5>
                 <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
@@ -87,10 +87,42 @@
             </div>
             </div>
 
-            <!-- === Producto 5 === -->
+            <!-- === Producto 5 gorra1=== -->
             <div class="col">
             <div class="card h-100">
-                <img src="assets/img/Gorra3.jpeg" class="card-img-top" alt="Remera 2">
+                <img src="assets/img/Gorras/GorraPrelavadaAzulMenta.jpeg" class="card-img-top img-producto" alt="Gorra Prelavada Azul Menta">
+                <div class="card-body">
+                <h5 class="card-title">Gorra Prelavada Azul Menta</h5>
+                <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
+                <p><strong>Precio:</strong> $3900</p>
+                <p><strong>Stock disponible:</strong> 20 unidades</p>
+                </div>
+                <div class="card-footer text-center">
+                <button class="btn btn-primary">Agregar al carrito</button>
+                </div>
+            </div>
+            </div>
+
+            <!-- === Producto 6 gorra2=== -->
+            <div class="col">
+            <div class="card h-100">
+                <img src="assets/img/Gorras/GorraPrelavadaGrisClaro.jpeg" class="card-img-top img-producto" alt="Gorra Prelavada Azul Menta">
+                <div class="card-body">
+                <h5 class="card-title">Gorra Prelavada Gris claro</h5>
+                <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
+                <p><strong>Precio:</strong> $3900</p>
+                <p><strong>Stock disponible:</strong> 20 unidades</p>
+                </div>
+                <div class="card-footer text-center">
+                <button class="btn btn-primary">Agregar al carrito</button>
+                </div>
+            </div>
+            </div>
+
+            <!-- === Producto 7 remera3=== -->
+            <div class="col">
+            <div class="card h-100">
+                <img src="assets/img/remeraT-shirtMesh.webp" class="card-img-top img-producto" alt="T-shirtMesh">
                 <div class="card-body">
                 <h5 class="card-title">Remera Blanca Clásica</h5>
                 <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
@@ -103,10 +135,10 @@
             </div>
             </div>
 
-            <!-- === Producto 6 === -->
+            <!-- === Producto 8 buzo3=== -->
             <div class="col">
             <div class="card h-100">
-                <img src="assets/img/Gorra3.jpeg" class="card-img-top" alt="Remera 2">
+                <img src="assets/img/HoodieNoirFadeOup.webp" class="card-img-top img-producto" alt="Hoodie Noir Fade Oup">
                 <div class="card-body">
                 <h5 class="card-title">Remera Blanca Clásica</h5>
                 <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
@@ -119,70 +151,7 @@
             </div>
             </div>
 
-            <!-- === Producto 7 === -->
-            <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/Gorra3.jpeg" class="card-img-top" alt="Remera 2">
-                <div class="card-body">
-                <h5 class="card-title">Remera Blanca Clásica</h5>
-                <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
-                <p><strong>Precio:</strong> $3900</p>
-                <p><strong>Stock disponible:</strong> 20 unidades</p>
-                </div>
-                <div class="card-footer text-center">
-                <button class="btn btn-primary">Agregar al carrito</button>
-                </div>
-            </div>
-            </div>
-
-            <!-- === Producto 8 === -->
-            <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/Gorra3.jpeg" class="card-img-top" alt="Remera 2">
-                <div class="card-body">
-                <h5 class="card-title">Remera Blanca Clásica</h5>
-                <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
-                <p><strong>Precio:</strong> $3900</p>
-                <p><strong>Stock disponible:</strong> 20 unidades</p>
-                </div>
-                <div class="card-footer text-center">
-                <button class="btn btn-primary">Agregar al carrito</button>
-                </div>
-            </div>
-            </div>
-
-            <!-- === Producto 9 === -->
-            <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/Gorra3.jpeg" class="card-img-top" alt="Remera 2">
-                <div class="card-body">
-                <h5 class="card-title">Remera Blanca Clásica</h5>
-                <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
-                <p><strong>Precio:</strong> $3900</p>
-                <p><strong>Stock disponible:</strong> 20 unidades</p>
-                </div>
-                <div class="card-footer text-center">
-                <button class="btn btn-primary">Agregar al carrito</button>
-                </div>
-            </div>
-            </div>
-
-            <!-- === Producto 10 === -->
-            <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/Gorra3.jpeg" class="card-img-top" alt="Remera 2">
-                <div class="card-body">
-                <h5 class="card-title">Remera Blanca Clásica</h5>
-                <p class="card-text">Ideal para combinar con cualquier outfit. Suave al tacto.</p>
-                <p><strong>Precio:</strong> $3900</p>
-                <p><strong>Stock disponible:</strong> 20 unidades</p>
-                </div>
-                <div class="card-footer text-center">
-                <button class="btn btn-primary">Agregar al carrito</button>
-                </div>
-            </div>
-            </div>
-
+           
             <!-- Podés seguir agregando más productos duplicando bloques como este -->
 
         </div>
