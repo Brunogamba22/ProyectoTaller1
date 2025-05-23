768fe557b342cdb3800410069af6742639c6563b
JonathanGarcia <martingarciayoni@gmail.com>
2025-05-23 17:30:20 -0300

    Commit con renormalización de archivos

--- app/Views/back/Panel.php
+++
@@ -1,4 +0,0 @@
-<div class="container mt-5">
-    <h2>Bienvenido al panel</h2>
-    <p>Hola, <?= session()->get('nombre') ?>. Estás logueado como perfil <?= session()->get('perfil_id') == 1 ? 'Administrador' : 'Cliente' ?>.</p>
-</div>
