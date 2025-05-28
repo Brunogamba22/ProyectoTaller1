c25d4dc118692bfa7d6749ddc79f818ffce53cb4
JonathanGarcia <martingarciayoni@gmail.com>
2025-05-28 15:50:26 -0300

    Cambios en nav y body

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -23,4 +23,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   </head>
 
-<body class="fondo-elegante">
+<body class="fondo-elegante" >
